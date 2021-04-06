5acf22f126503cb035debc0ad36e76bbc7d5fd7d
DavinchiDev <80538770+DavinchiDev@users.noreply.github.com>
2021-04-06 01:44:54 -0500

    some changes
    *implementación de librería JS ScrollReveal
    *algunas adiciones

---
+++ index.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+    sr.reveal('.grid-container', {
+        duration: 4500
+    });
+
+window.sr = ScrollReveal();
+    sr.reveal('.grid-container2', {
+        duration: 3000
+    });
+
+window.sr = ScrollReveal();
+    sr.reveal('.notice', {
+        duration: 3000
+    });
+
+window.sr = ScrollReveal();
+    sr.reveal('.all-form-container', {
+        duration: 3000
+    })
---
+++ main.js
@@ -0,0 +1,4 @@
+alert("bienvenido, esta pagina es meramente informativa, no infringimos derechos de autor :)")
+
+
+
